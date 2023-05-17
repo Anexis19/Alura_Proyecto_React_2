05c3ce0ee6035255f5df24df9e6adfd37a7175d5
Andres Felipe Hurtado Zuñiga <Andres Hurtado>
2023-05-17 00:03:12 -0500

    Update. Aplicacion de estilos al componente de formulario. Se procede a crear un nuevo componente le cual sera reutilizado para la seccion de los inputs y labels que hacen parte de este componente Formulario

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 // Importacion del componente Header
 import Header from './components/Header/header';
+import Formulario from './components/Formulario/formulario';
 
+// Definicion de componente Padre. Componente APP es el componente principal
 function App() {
   return (
-    <div className="App">
+    <div>
       {/* Llamado a componente Header el cual se convierte a un elemento JSX */}
+
       <Header></Header>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo con React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+      <Formulario></Formulario>
+
     </div>
   );
 }
---
+++ src/components/Formulario/formulario.js
@@ -0,0 +1,13 @@
+// Importacion de Archivo CSS
+import "./formulario.css"
+// Creacion de arrow function
+const Formulario = () =>{
+    // Recordar que un componente se conoce como una funcion JS que retorna un elemento HTML
+    return <section className="formulario">
+        <form>
+            <h2>Rellena el formulario para crear el colaborador</h2>
+        </form>
+    </section>
+}
+// Exportacion del componente
+export default Formulario
