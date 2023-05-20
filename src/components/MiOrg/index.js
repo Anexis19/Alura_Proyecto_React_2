c21676d871d099309144be891bf8da3f9f4de750
Andres Felipe Hurtado Zuñiga <Andres Hurtado>
2023-05-19 23:38:10 -0500

    Update. Aprendizaje en el uso de los estado en React. Se hace uso de useState, el cual almacena un valor inicial de una variable y que puede ser actualizado segun se desee. Revisar documentacion que hay en el archivo index del componente MiOrg

--- src/components/MiOrg/index.js
+++ src/components/MiOrg/index.js
@@ -1,9 +1,34 @@
+import { useState } from "react"
 import "./MiOrg.css"
 
+
 const MiOrg =() =>{
-   return <section className="orgSection">
-        <h3 className="title">Mi Organización</h3>
-        <img src="/img/add.png" alt="Error al cargar el boton add" />
-   </section>
+
+    // Estado - Hooks
+    /*
+        useState = Usar el estado
+        Funcion que recibe como parametro, el valor inicial de un estado
+        El cual retorna dos elementos
+
+        const [nombreVariable, funcionActualiza] = useState(valorInicial)
+    */
+
+    // La variable mostrar, tiene un estado inicial de TRUE
+    const [mostrar,actualizarMostrar] = useState(true)
+
+    // Funcion manejarClick que realiza la actualizacion del estado de la vartiable MOSTRAR
+    const manejarClick = () =>{
+        console.log("Mostrar/Ocultar", !mostrar);
+        // Actualizacion de Estado. Pasa de TRUE a FALSE y de FALSE a TRUE
+        actualizarMostrar(!mostrar)
+    }
+
+    return <section className="orgSection">
+            <h3 className="title">Mi Organización</h3>
+            {/* Se le agrega la propiedad onClick la cual, al ejecutarse el evento click
+                manda a llamar la funcion manejarClick
+            */}
+            <img src="/img/add.png" alt="Error al cargar el boton add" onClick={manejarClick}/>
+    </section>
 }
 export default MiOrg
